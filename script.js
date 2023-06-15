af2b1b6f41e552b01962e08d77f76a586babb833
Khushboo Dalal <khushboodalal1434@gmail.com>
2023-06-15 19:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort((a , b)=>a-b);
+
+	let n = arr.length;
+	let closestSum = Infinity;
+
+	for(let i=0; i<n-2; i++){
+		let left = i+1;
+		let right = n-1;
+		while(left<right){
+			let sum = arr[i]+arr[left]+arr[right];
+
+			if(Math.abs(target-sum) < Math.abs(target-closestSum)){
+				closestSum = sum;
+			}
+
+			if(sum < target){
+				left++;
+			}
+			else{
+				right--;
+			}
+		}
+	}
+		return closestSum;
   
 }
 
